a5877ec4310e9b8823313be231c20b2c6ef797d1
Taro Morita <morita0079@gmail.com>
2022-07-15 04:01:22 +0900

    リファクタ

--- measurement.py
+++ measurement.py
@@ -3,14 +3,24 @@ import re
 
 
 class Measurement:
+    def __init__(self) -> None:
+        self.device = "/sys/bus/w1/devices/28-3c01f0952927/w1_slave"
+
     def measure(self):
-        device = "/sys/bus/w1/devices/28-3c01f0952927/w1_slave"
-        with open(device) as f:
+        with open(self.device) as f:
             lines = f.readlines()
-            line = lines[1]
-            pattern = re.compile(r"t=\d+")
-            matched_text = pattern.search(line)
-            text = matched_text.group()
-            stliped = text.replace('t=', '')
-            result = float(stliped) / 1000
+            stliped = self.__extract_temperature(lines)
+            result = self.__calculate_temperature(stliped)
         return result
+
+    def __calculate_temperature(self, pre_text):
+        result = float(pre_text) / 1000
+        return result
+
+    def __extract_temperature(self, lines):
+        line = lines[1]
+        pattern = re.compile(r"t=\d+")
+        matched_text = pattern.search(line)
+        text = matched_text.group()
+        stliped = text.replace('t=', '')
+        return stliped
