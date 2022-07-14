c452e7c7963e28d0a75416977cec9bfa1d4e7c20
Taro Morita <morita0079@gmail.com>
2022-07-15 03:11:27 +0900

    温度を出力

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ device = "/sys/bus/w1/devices/28-3c01f0952927/w1_slave"
 while True:
     with open(device) as f:
         lines = f.readlines()
-        print(lines)
+        line = lines[1]
+        pattern = re.compile(r"t=\d+")
+        matched_text = pattern.search(line)
+        text = matched_text.group()
+        stliped = text.replace('t=', '')
+        result = float(stliped) / 1000
+        print(result)
